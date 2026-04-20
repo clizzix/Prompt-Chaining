0c666b10082b15c3a968770c8cd0ab9887d59c32
clizzix <godsilla1993@gmail.com>
2026-04-20 22:03:40 +0200

    removed unused openai setup from utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
-import type { Response } from 'express';
-import type { ChatCompletionCreateParamsNonStreaming } from 'openai/resources';
-import OpenAI from 'openai';
 import Pokedex, { type Pokemon } from 'pokedex-promise-v2';
 import type { ErrorResponseDTO } from '#types';
 
@@ -18,32 +15,32 @@ export const returnError = async ({ message }: { message: string }): Promise<Err
   };
 };
 
-export const createOpenAICompletion = async (
-  client: OpenAI,
-  res: Response,
-  llmRequest: ChatCompletionCreateParamsNonStreaming,
-  stream: boolean
-) => {
-  if (stream) {
-    const completion = await client.chat.completions.create({
-      ...llmRequest,
-      stream
-    });
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
+// export const createOpenAICompletion = async (
+//   client: OpenAI,
+//   res: Response,
+//   llmRequest: ChatCompletionCreateParamsNonStreaming,
+//   stream: boolean
+// ) => {
+//   if (stream) {
+//     const completion = await client.chat.completions.create({
+//       ...llmRequest,
+//       stream
+//     });
+//     res.setHeader('Content-Type', 'text/event-stream');
+//     res.setHeader('Cache-Control', 'no-cache');
+//     res.setHeader('Connection', 'keep-alive');
 
-    for await (const part of completion) {
-      if (part.choices[0]?.delta?.content) {
-        res.write(`data: ${part.choices[0].delta.content}\n\n`);
-      }
-    }
-    res.end();
-    return;
-  } else {
-    const completion = await client.chat.completions.create(llmRequest);
-    res
-      .status(200)
-      .json({ completion: completion.choices[0]?.message.content || 'No completion generated' });
-  }
-};
+//     for await (const part of completion) {
+//       if (part.choices[0]?.delta?.content) {
+//         res.write(`data: ${part.choices[0].delta.content}\n\n`);
+//       }
+//     }
+//     res.end();
+//     return;
+//   } else {
+//     const completion = await client.chat.completions.create(llmRequest);
+//     res
+//       .status(200)
+//       .json({ completion: completion.choices[0]?.message.content || 'No completion generated' });
+//   }
+// };
